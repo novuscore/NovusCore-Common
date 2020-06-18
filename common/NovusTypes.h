99eccfadadd9f0e5ebf961434205edf03290c473
Pursche <pursche01@gmail.com>
2020-06-18 22:27:49 +0200

    Add unsigned int versions of vectors to NovusTypes.h

--- common/NovusTypes.h
+++ common/NovusTypes.h
@@ -43,8 +43,11 @@ typedef double f64;
 
 using vec2 = glm::vec2;
 using ivec2 = glm::ivec2;
+using uvec2 = glm::uvec2;
 using vec3 = glm::vec3;
 using ivec3 = glm::ivec3;
+using uvec3 = glm::uvec3;
 using vec4 = glm::vec4;
 using ivec4 = glm::ivec4;
+using uvec4 = glm::uvec4;
 using mat4x4 = glm::mat4x4;
